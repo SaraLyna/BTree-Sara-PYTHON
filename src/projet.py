bb5ceb77844f49a1df400fbdb2c26a72406dda4a
Ouyahia Sara-lyna <saralyna.ouyahia.etu@a12p2.fil.univ-lille.fr>
2024-01-10 09:34:22 +0100

    premiere seance

---
+++ src/projet.py
@@ -0,0 +1,4 @@
+ # @author Sara Lyna OUYAHIA
+ # @date 10/01/2024
+ 
+ # run the application window+tree_B
---
+++ src/window.py
@@ -0,0 +1,5 @@
+ # @author Sara Lyna OUYAHIA
+ # @date 10/01/2024
+ 
+ 
+ # create the window and the toolBar
