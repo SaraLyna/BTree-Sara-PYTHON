b5989bfd0182f3db22ca6cfae8de6010f531da05
Ouyahia Sara-lyna <saralyna.ouyahia.etu@a12p2.fil.univ-lille.fr>
2024-01-17 10:01:02 +0100

    suppression de la classe MyTree.py

--- src/MyTree.py
+++
@@ -1,9 +0,0 @@
- # @author Sara Lyna OUYAHIA
- # @date 10/01/2024
- 
- # cette classe hérite de la classe générique Tree-Balanced
- # le polymorphisme : surcharger les méthodes de Tree-Balanced
- 
- class MyTree(TreeBalanced):
-    def __init__(self):
-       super().__init__()
