e30b6cd897b5dec186ba5c6c98cc908620da36ad
Ouyahia Sara-lyna <saralyna.ouyahia.etu@a12p17.fil.univ-lille.fr>
2024-01-24 09:27:31 +0100

    mise à jour

--- src/TreeBalanced.py
+++ src/TreeBalanced.py
@@ -20,42 +20,47 @@ class TreeBalanced:
         
 
 
-    def add_node(self, node):
-        if self.root is None:
-            self.root = node
-        else:
-            self._insert_node(node, self.root)
+#     def add_node(self, node):
+#         if self.root is None:
+#             self.root = node
+#         else:
+#             self._insert_node(node, self.root)
 
 
 
-    def search_value(self, key, node):
-        if node is None:
-            return None
-        elif key in node.keys:
-            index = node.keys.index(key)
-            if 0 <= index < len(node.childs):
-                return node.childs[index]
-            else:
-                return None
-        else:
-            for child in node.childs:
-                result = self.search_value(key, child) 
-                if result is not None:
-                    return result
+    def search(self, key, node=None):
+        if not node:
+            node = self.root
+        i = 0
+        while i < len(node.keys) and key > node.keys[i]:
+            i += 1
 
-            
-            
-    def search_bool(self, key, node):
-        if node is None:
-            return False
-
-        if key in node.keys:
+        if i < len(node.keys) and key == node.keys[i]:
             return True
+        elif len(node.childs) > 0:
+            print(f"Descending to child {i}")
+            return self.search(key, node.childs[i])
         else:
-            for child in node.childs:
-                if self._search_bool(key, child):
-                    return True
+            print("Key not found")
             return False
+        
+    def linearize(self, node=None):
+        if not node:
+            node = self.root
+
+        res = []
+        for i in range(len(node.keys)):
+            res.append(node.keys[i])
+            if len(node.childs) > i:
+                res += self.linearize(node.childs[i])
+
+        if len(node.childs) > len(node.keys):
+            res += self.linearize(node.childs[-1])
+
+        return res
+
+
+            
 
 
 
@@ -72,42 +77,57 @@ class TreeBalanced:
 
         return True
 
-# tree = TreeBalanced(degree=3)
-# tree.root = node4 
-# # Adding nodes
+# Adding nodes
 
-# node2 = Node(1, "A")
-# node5 = Node(2, "B")
-# node7 = Node(3, "C")
-# node10 = Node(4, "D")
-# node12 = Node(5, "E")
-# node15 = Node(6, "F")
-# node20 = Node(7, "G")
+node1 = Node(1, "A")
+node2 = Node(2, "B")
+node3 = Node(3, "C")
+node4 = Node(4, "D")
+node5 = Node(5, "E")
+node6 = Node(6, "F")
+node7 = Node(7, "G")
 
-#  
-# node4.childs = [node2, node6]
-# node2.childs = [node1, node3]
-# node6.childs = [node5, node7]
-#  
-# # Searching for values
+tree = TreeBalanced(degree=3)
+tree.root = node4
 
+node4.childs = [node2, node6]
+node2.childs = [node1, node3]
+node6.childs = [node5, node7]
+ 
 
-# key_to_search = 5
-# result_node = tree.search_value(key_to_search, tree.root)
+# tree2 = TreeBalanced(degree=2)
+# tree2.root= node7
 # 
-# if result_node is not None:
-#     print(f"Node with key {key_to_search} found: {result_node}")
-# else:
-#     print(f"Node with key {key_to_search} not found.")
+# node7.childs= [node1, node2]
+# node1.childs= [node4]
+# node2.childs= [node5, node6, node3]
+
+key_to_search = 5
+result_node = tree.search(key_to_search, tree.root)
+
+if result_node is not None:
+    print(f"Node with key {key_to_search} found: {result_node}")
+else:
+    print(f"Node with key {key_to_search} not found.")
     
-# print(tree.search_value(4, tree.root))  # Output: 'D'
-# print(tree.search_value(7, tree.root))  # Output: 'G'
-# print(tree.search_value(2, tree.root))  # Output: 'B'
-#  
-#  # Searching for keys
-# print(tree.search_bool(6, tree.root))  # Output: True
-# print(tree.search_bool(25, tree.root))  # Output: False
-#  
-#  # Checking if it's a B-tree
-# is_balanced = tree.is_Btree(tree.root)
-# print(f"Is the tree balanced? {is_balanced}")
+print(tree.search(4, tree.root))  # Output: True
+print(tree.search(8, tree.root))  # Output: False
+print(tree.search(2, tree.root))  # Output: True
+ 
+ # Checking if it's a B-tree
+is_balanced = tree.is_Btree(tree.root)
+print(f"Is the tree balanced? {is_balanced}")
+
+# not_balaned= tree2.is_Btree(tree2.root)
+# print(f"Is the tree balanced? {not_balanced}")
+# 
+# result1 = sorted(tree.linearize()) == tree.search(4, tree.root)
+# result2 = sorted(tree.linearize()) == tree.search(8, tree.root)
+# result3 = sorted(tree.linearize()) == tree.search(13, tree.root)
+# result4 = sorted(tree.linearize()) == tree.search(1)
+# 
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
+# 
